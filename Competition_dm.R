05da17a5a58e851153675f084d129982966c0c89
davemakowski <79920404+davemakowski@users.noreply.github.com>
2021-03-02 15:19:21 +0100

    Create Competition_dm.R

---
+++ Competition_dm.R
@@ -0,0 +1,91 @@
+
+# modify your working directory with the data file if necessary (using the setwd() command):
+
+# read data:
+load("data_train.RData")
+
+# show severity thresholds:
+u_ba # for BA
+u_cnt # for CNT
+# show weights used for the weighted RPS:
+weights_ba
+weights_cnt
+
+# explore the data:
+dim(data_train_DF)
+names(data_train_DF)
+
+# data points to predict:
+sum(is.na(data_train_DF$BA))
+sum(is.na(data_train_DF$CNT))
+
+# histogram of training data on log-scale for better readability:
+hist(log(1+data_train_DF$CNT), xlab = "log(1+CNT)", main = "Histogram of log-transformed CNT")
+hist(log(1+data_train_DF$BA), xlab = "log(1+BA)", main = "Histogram of log-transformed BA")
+
+
+# Example predictions (using the benchmark models): ####
+
+# CNT (Poisson regression): ####
+
+# remove rows with data to predict:
+train_DF = data_train_DF[!is.na(data_train_DF$CNT),]
+test_DF = data_train_DF[is.na(data_train_DF$CNT),]
+# remove BA (which contains more NA values):
+train_DF = subset(train_DF, select = -BA)
+# in test data, remove BA and the NA column of CNT:
+test_DF = subset(test_DF, select = -c(CNT, BA))
+dim(train_DF)
+dim(test_DF)
+dim(train_DF) # dimension of training data
+# train the model (Poisson regression with all covariates):
+fit = glm(CNT ~ ., data = train_DF, family = poisson(link = "log"))
+summary(fit)
+
+# calculate estimated Poisson intensities (=means of predictive distributions):
+pred_mean_cnt = predict(fit, test_DF, type = "response")
+# calculate the matrix with estimated exceedance probability of the severity thresholds:
+prediction_cnt = matrix(nrow = 80000, ncol = length(u_cnt))
+for(k in 1:length(u_cnt)){
+  prediction_cnt[,k] = ppois(u_cnt[k], lambda = pred_mean_cnt)
+}
+# prediction_cnt has to be submitted for the competition
+
+# explore the mean of estimated probabilities for each severity threshold:
+apply(prediction_cnt, 2, mean)
+
+#
+# BA (log-normal regression): ####
+# Procedure: we here fit the log-normal model only to positive values of BA, and then combine this with the count model above for the probability of BA=0.
+# remove rows with data to predict:
+train_DF = data_train_DF[!is.na(data_train_DF$BA),]
+test_DF = data_train_DF[is.na(data_train_DF$BA),]
+# remove CNT (which contains more NA values):
+train_DF = subset(train_DF, select = -CNT)
+# # in test data, remove CNT and the NA column of BA:
+test_DF = subset(test_DF, select = -c(CNT, BA))
+dim(train_DF)
+dim(test_DF)
+# use only positive BA values for fitting the log-normal GLM:
+train_DF = train_DF[train_DF$BA > 0 ,]
+# train the model:
+fit = lm(log(BA) ~ ., data = train_DF) 
+summary(fit)
+
+# extract the predictive standard deviation:
+sd_gauss = sd(fit$residuals)
+sd_gauss
+
+# extract predicted Gaussian means of BA>0:
+pred_mean_logba = predict(fit, test_DF)
+hist(pred_mean_logba)
+# calculate estimated exceedance probabilities for the BA component:
+prediction_ba = matrix(nrow = 80000, ncol = length(u_ba))
+for(k in 1:length(u_ba)){
+  # we here use prediction_cnt[,1] for the probability P(BA = 0).
+  prediction_ba[,k] = prediction_cnt[,1] + (1-prediction_cnt[,1]) * pnorm(log(u_ba[k]), mean = pred_mean_logba, sd = sd_gauss)
+}
+# prediction_ba has to be submitted for the competition
+
+# explore the mean of estimated probabilities for each severity threshold:
+apply(prediction_ba, 2, mean)
